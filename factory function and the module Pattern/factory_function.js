a63eb71d9fe8f0c3fc7ff1e98974c8d40e9a69e5
upendradhami <upendradhami689@gmail.com>
2025-04-02 07:08:05 +0545

    added factory_function

--- factory function and the module Pattern/factory_function.js
+++ factory function and the module Pattern/factory_function.js
@@ -5,14 +5,46 @@
      name = name;
      email = '@'+name;
      let count = 0;  // this is called a private variable .
-     let reputaion = () =>  count++ ;
-     let givereputation = () => count++;
+     let reputaion = () => { count++ ; return count;};
+     let  givereputation = () => { count++  ; return count; };
      return {name,email,reputaion,givereputation};
  }
 
-let obj1 = greet('ram');
+ let obj1 = greet('ram');
  console.log(obj1);
  console.log(obj1.email);
  console.log(obj1.reputaion());
  console.log(obj1.givereputation());
  
+
+ //another example;
+
+//  const Anything = function (){
+
+//     let count = 0;    // this is private variable 
+
+//     function showCount(){       // this is a private function as it is not returned as object
+//         console.log(`your count is ${count}`);
+//     }
+
+//     return{
+//         increase(){
+//         count++;
+//         showCount();},    //private functions are accessed by these functions 
+//        decrement(){
+//         count--; 
+//         showCount()},
+//        getcount(){
+//         return count;
+//     }
+//  }; 
+
+// }
+
+// let first = Anything();
+// //  first.showCount();  // show count cannot be accessed because it is a private function and not returned as object  ERROR !!! 
+// first.increase();
+// first.increase();
+// first.decrement();
+
+// console.log(first.getcount());
