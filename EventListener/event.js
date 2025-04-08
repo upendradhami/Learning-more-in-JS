c7e4f54777e35bfb3edf65ef560afbf995fd1d8d
upendradhami <upendradhami689@gmail.com>
2025-04-08 06:41:47 +0545

    capture,bubble,mousemove,mouseenter,this

--- EventListener/event.js
+++ EventListener/event.js
@@ -45,20 +45,20 @@
 // ========================================================
 let buyButton = document.querySelectorAll('button.buy');
 
-buyButton.forEach (function(button) {
- button.addEventListener('click', handleButtonClick);
-},true);
-
+buyButton.forEach(function(event){
+  event.addEventListener('click',handleButtonClick);
+})
 
 // by passing the even in the event function we are allowed to access the various method connected with the event like event.target , event.buttons , .clientY , .defaultPrevented , .currentTarget)
 
 function handleButtonClick(event){
 
- console.log(event.currentTarget);
-  console.log(event.target); 
-  console.log(event.target === event.currentTarget); 
-  console.log(event.target.innerText);event
-  event.stopPropagation();
+//  console.log(event.currentTarget);
+//   console.log(event.target); 
+// console.log(event.target === event.currentTarget); 
+  console.log('you have clicked on the button');
+  console.log(event.target.innerHTML);
+  // event.stopPropagation();
   //  console.log(event.clientY);
   // console.log(event.target.dataset);
   // console.log(event.target.className);
@@ -69,15 +69,30 @@ function handleButtonClick(event){
 
 window.addEventListener('click',function (event) {
   console.log("clicked the window from anywhere ");
+  event.stopPropagation();
+},{capture:true});
+
+// this {capture:true} is enabled by disabling the event.stoppropagation() above in handleButtonClick and hence , what it does is , first it always show the capture mode i.e clicking is starting from window, document, html ,body, button...... iin this way.
+
+
+// if we add event.stopPropagation() in this window.addEventListener , it will stop progagation to go down flow  . now no other button click will get blocked .
+
+
+// Captures goes down and bubble goes up (default)  and event.stopPropagation stop both of them at step 1st of either capture or bubble.
+
+let photo1 = document.querySelector('.photo');
+photo1.addEventListener('mouseenter',function(e){
+  console.log(e.target);
+  console.count(this);
 });
 
 
+// this mousemove event calls the eventlistener millions of time when we move our mouse
+// we also have mouseenter event which fires up when we enter and leave the image 
 
 
-
-
-
-
+// what happens if we console log (this ) in the photo1 above let's see.
+// it will perform similar to that of the console.log(e.target);
 
 
 
