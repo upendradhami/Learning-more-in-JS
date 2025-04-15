e6853a7f0609c7ed3c9e2d59b1b53ca801605cb8
upendradhami <upendradhami689@gmail.com>
2025-04-15 12:34:01 +0545

    skt methd and coding challenge

---
+++ dataStructureOperator/codingChallenge.js
@@ -0,0 +1,66 @@
+// Initial JavaScript File
+'use strict';
+
+(function() {
+  // work with the given data  below and do the tasks .
+  const game = {
+    team1 : 'Babal Mahendranagar' ,
+    team2 : 'Ramailo Rajipur',
+    Players: [
+      [
+        'Upendra', 'Ram', 'Shyam', 'Hari', 'Kale', 'Thyapchey', 'Nyaptey', 'Kupre', 'Xidrea', 'Gwachey', 'Losey'
+      ],
+      
+      ['Dhami', 'Ramesh', 'Samir', 'Harish', 'Krishna', 'Tiru', 'Naren', 'Kamal', 'Xapey', 'Ganesh', 'Lokesh']
+    ],
+    score: '4,0',
+    scored: ['Losey', 'Gwachey', 'Losey', 'Hari'],
+    date: 'April 15th 2026',
+    odds: {
+       team1 : 1.44,
+       x: 3.42,
+       team2: 6.5
+    },
+  };
+
+ // 1. create one player array for each team 
+  let [player1 ,player2 ] = [...game.Players];
+  console.log(...player1,...player2);
+
+ // 2. create one varieble goal keeper and others for others in a team1
+let [gk , ...others] = player1;
+console.log(gk, others);
+
+ // 3. create an array of allPlayers fro all 22 players ;
+ let allPlayers = [...player1, ...player2];
+ console.log(allPlayers);
+ 
+
+ //4. during game team1 added 3 players so create new array players1Final containing all plus 'tekendra','chakra' , 'prakash'
+ let players1Final = [...player1 , 'tekendra ' , 'chakra' , 'prakash'];
+ console.log(players1Final);
+
+ //5. from game.odds objects make 3 variables team1 , draw , team2 
+ let {odds : {team1, x:draw , team2}} = game;
+ console.log(team1, draw , team2);
+
+
+ //6. make a function which takes the no. of players (not array ) and console them and show the goals scored as per the no. of players 
+ function printGoals(...players){
+  players.forEach(play => {console.log(`${play}`);});
+  console.log(`no. of goals scored: ${players.length}`);
+ }
+ printGoals('upendra ' , 'tekendra' , 'losey');
+ printGoals('upendra' , 'ram ' , 'upendra' , 'shyam ' , 'hari');
+ printGoals(...game.scored);   // destructured the array scored and send it to function
+
+ // team with lower odd is more likely to win so print console which team is  more likely to win 
+ console.clear();
+
+ let oka=9;
+ // if false , console (execute)
+ team1 > team2 || console.log('team1 wins due to little odd');
+
+ // if true , console ( execute)
+ team1 < team2 && console.log("team1 wins due to little odd");
+})();
--- dataStructureOperator/srtCKT.js
+++ dataStructureOperator/srtCKT.js
@@ -1,25 +1,52 @@
-console.log('------OR-------');
+// Initial JavaScript File
+'use strict';
+
+(function() {
+  console.log('------OR-------');
 console.log(0 || true);
 console.log(undefined || null);
 console.log(0 || undefined || null || 90 || 'upendra ' );   // this shorkciruit display s the first true value found among numbers of false value and true value oring 
 
-let guestNumber;
-
-let guest = guestNumber ? guestNumber : 10;
+let guestNumber =5;
+// let guest = guestNumber ? guestNumber : 10;
+let guest = guestNumber || 10;                   
 console.log(guest);  // this equals  below statement: 
+
 
 guestNumber = 23;
 console.log(guestNumber || 10);  //if true execute the guestNUmber else 10.
 
+
+// ============================================================================================
+
 console.log('----------AND-----------------');
 console.log('kaliya naag ' && 'upendra dhammi' && 89); // compares all the numbers and if all are true then gives the last one 
-console.log('upendra' && 89 && undefined && 0); // gives the first false value ;
-console.log(true && 89 && false);
+console.log('upendra' && 89 && undefined && 0); // gives the first false value when found;
+console.log(false && 89 && true);  // return false false value at the first and leave other 
 
 let makepizza = function() {
   console.log('you can make pizza ');
 } ;
-let orderpizza = makepizza ? makepizza() : 10;
 
-console.log(makepizza() && 34);
+if(makepizza){
+  makepizza();
+}
 
+makepizza && makepizza();      // both works the same && operator goes to the last value if all true 
+console.clear();
+
+// Nullish value ?? 
+// ===========================================it takes
+// 'NULL and UNDEFINED ' only as fasly values but not 0 
+ 
+let Number = 0;
+// let guest = guestNumber ? guestNumber : 10;
+let guests = Number || 10;         // operator || will treat 0 and '' as false value  
+ console.log(guests);         
+ guests = Number && 10;         // operator || will treat 0 and '' as false value    
+ console.log(guests);       
+ guests = undefined ?? Number ??10;         // operator || will treat 0 and '' as true value          
+console.log(guests); 
+
+
+})();
