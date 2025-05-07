655768ec35f88a57b4f77124bb132401f6cc674b
upendradhami <upendradhami689@gmail.com>
2025-05-07 20:44:28 +0545

    style, attribute and class in DOM

--- The DOM/Advanced Topic/script.js
+++ The DOM/Advanced Topic/script.js
@@ -29,3 +29,64 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+// Learnings are all here ==========================================================================
+
+// //  SELECTING THE ELEMENT 
+// console.log(document.documentElement); // selects the whole document.
+// console.log(document.head);
+// console.log(document.body);
+
+const header =document.querySelector('.header');   // selects the first  element with the header class 
+// console.log(document.querySelectorAll('.header'));  // returns a nodelist with the class header elements 
+// console.log(document.getElementById('logo'));  //selects by id
+// console.log(document.getElementsByClassName('highlight')); // return  a HTMLCollection with the highlight classname
+// console.log(document.getElementsByTagName('button')); // also returns a HTMLcollection having the tagname button
+
+// ================================== creating and deleting elements =------=--------=-============
+
+// creating ---
+ let cookie = document.createElement('div');
+ cookie.classList.add('cookie-message');
+ cookie.textContent = ' HEre are some coookies if you want to add ';
+ cookie.innerHTML = ` Here are some coookies if you want to add     <button class ='btn btn--close'>close</button>`;
+
+ // inserting into document
+
+//  header.prepend(cookie);
+ header.append(cookie);  // prepend and append are methods for inserting element as child 
+ //header.append(cookie.cloneNode(true));  // allow the cookie to be added into another place without removal of it .ie copy the element 
+
+//  header.prepend(cookie);
+
+//  header.before(cookie); 
+//  header.after(cookie);  // it insert element as sibling
+
+// deleting the element
+  document.querySelector('.btn--close').addEventListener('click',function() {
+    cookie.remove();
+  });
+
+// ============================================  STYLES , ATTRIBUTES AND CLASSES ============================================  
+
+// styles
+ cookie.style.backgroundColor = '#37383d';
+ cookie.style.width = '120%';
+ 
+ // if we want to access the style attached to the class (cookie-message) of element cookie . we get nothing 
+ console.log(cookie.style.color);
+ console.log(cookie.style.fontSize);
+ console.log(cookie.style.backgroundColor);
+
+ // to get such values we  have : 
+ console.log(getComputedStyle(cookie).color);
+ console.log(getComputedStyle(cookie).fontSize);
+
+ // now if we want to modify that value we proceed as : 
+  cookie.style.height = Number.parseFloat(getComputedStyle(cookie).height) + 40 + 'px' ; 
+
+ // If we have to change the style of already existing css variable then : 
+  document.documentElement.style.setProperty('--color-primary', 'lightblue');
+  
+
