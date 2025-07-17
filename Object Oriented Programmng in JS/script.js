c6d773e841d03fad8641fd4ff1caecb5b19a9866
Upendra Dhami <upendradhami.becomp@2079.edu.np>
2025-07-17 20:08:18 +0545

    class inheritance using Object.create()

--- Object Oriented Programmng in JS/script.js
+++ Object Oriented Programmng in JS/script.js
@@ -1,7 +1,7 @@
 `use strict`;
 (function() {
 
-  // constructor function
+  //=============================================== constructor function===============================
 const Person  = function(username, birthDate){
   this.username = username;
   this.birthDate = birthDate;
@@ -11,8 +11,10 @@ const Person  = function(username, birthDate){
 //     console.log( `${this.username} your Age is = ${2025 - this.birthDate}`);}
 }
 
-const Upendra =new Person ('Upendra', 2003);
-//1. 'new' operator creates an empty new object/{}
+const Upendra =new Person ('Upendra dhami', 2003);
+
+//======================================================================
+// 1. 'new' operator creates an empty new object/{}
 //2. function is called , this == object created
 //3. new object {}  is linked to prototype
 //4. function is automatically returned to new {}
@@ -34,6 +36,7 @@ Person.prototype.calAge = function() {
 
 // console.log(Upendra instanceof Person );
 
+// //===================================================================================
 // // __proto__ is used by any object to link itself with the object prototype 
 // // In Person.prototype , prototype is not of the Person  but it is of the main object which is linked to all the objects created.
 
@@ -43,39 +46,48 @@ Person.prototype.calAge = function() {
 
 // console.log(Object.prototype.isPrototypeOf(Person));
 
-// // linked to object prototype
+// //====================================== linked to object prototype
 // Person.prototype.species = " Homo Sapiens";
 // console.log(Upendra.species, Aaradhya.species);
 // console.log(Upendra.hasOwnProperty('username'));
 //  // console.log(Upendra.hasOwnProperty('species'));
 
-// // let's see prototypal inheritance chain
+// //================================== let's see prototypal inheritance chain
 // console.log(Aaradhya.__proto__);
 // console.log(Aaradhya.__proto__.__proto__);
 // console.log(Aarshabh.__proto__.__proto__.__proto__);
 
-// // this will show you whole Person's definition
+// //=============================== this will show you whole Person's definition
 // console.dir(Person.prototype.__proto__.constructor);
 // console.dir(Aarshabh.__proto__.constructor
 // );
 
 
-// // now let's see the prototype of  of the array 
+// //======================================= now let's see the prototype of  of the array 
 //  let arr = [2,2,3,5,9,9,7,7];
 //  console.log(arr.__proto__ === Array.prototype);
 //  console.log(arr.__proto__);
  
-//  // we  can set our own properties or method at the main prototype but it is not recommmended 
+//  //========================= we  can set our own properties or method at the main prototype but it is not recommmended 
 
 //  Array.prototype.unique = function()  {
 //   return [...new Set(this)];
 //  };
 //  console.log(arr.unique());
  
-//  // now for fun let's see how is this H1 's prototype chain is moving over 
+//  //============================== now for fun let's see how is this H1 's prototype chain is moving over 
  
 //  console.dir(document.querySelector('h1').__proto__)
 
+
+// ================================ STATIC METHOD ================================================
+// it is used only for the case if you want any single function for the object
+// Static method assigned directly to the constructor
+Person.getSurname = function(username) {
+  // Split the username by spaces and return the last element
+  const parts = username.trim().split(" ");
+  return parts.length > 1 ? parts[parts.length - 1] : "";
+};
 
 
 ///////////////////////////////////////
@@ -117,37 +129,92 @@ Person.prototype.calAge = function() {
 // console.log(car2.brake());
 
 
-// ES6 classes 
+// // =============================================================ES6 classes 
   
-// class expression 
-  // const PersonCl = class{}
+// //=========================== class expression 
+//   // const PersonCl = class{}
 
-// class declaration 
- class PersonCl{
-  constructor(firstName, DOB){
-    this.firstName = firstName;
-    this.DOB = DOB;
-  }
+// //============================== class declaration 
+//  class PersonCl{
+//   constructor(firstName, DOB){
+//     this.firstName = firstName;
+//     this.DOB = DOB;
+//   }
 
-  // calAge(){
+//   // calAge(){
     
-  // }
+//   // }
+//  }
+
+
+//  //================= calling is similar to objects creation using 'new' keyword
+//   const diyan = new PersonCl('Diyan', 2024);
+//   console.log(diyan);
+  
+//   // .................adding function using prototype.........
+//   PersonCl.prototype.calAge = function(){
+//     this.DOB = 2025- this.DOB;
+//     console.log(`${this.firstName} , your age is : ${this.DOB}`);
+//   }
+
+//   console.log(PersonCl.__proto__);
+//   diyan.calAge();
+
+//  // =========================== GETTERS AND SETTERS ===================================
+//    const car ={
+//     name: "roylles royce",
+//     price: "10000$",
+//     version: [3.4 , 4.2 , 5 , 6],
+
+//    ///.,.........getter............
+//     get latestVersion(){
+//       return this.version.slice(-1);
+//     },
+//    ///............setter............
+//     set latestVersion(value){
+//       this.version.push(value);
+//     }
+
+//    }
+  
+//    console.log(car.latestVersion);
+//    car.latestVersion= 77;
+//    console.log(car.latestVersion);
+
+//--------------=================OBject.CReate()---------===========================
+const clock = {
+  call: ()=>{
+    console.log(`It is exactly ${new Date().getHours()}o'clock now.`);
+  },
+  setalarm: (time,merdian)=>{
+     console.log(`you have set an ,alarm for ${time} ${merdian}`)
+  }
+  
  }
 
+ const Alarm = Object.create(clock);  // we use this instead of new operator for creating object
+ Alarm.setalarm(7,'am');
+ Alarm.call();
 
- // calling is similar to objects creation using 'new' keyword
-  const diyan = new PersonCl('Diyan', 2024);
-  console.log(diyan);
-  
+ // Let's now perform inhertance of student object from the object Person 
+ 
+ 
+ const Student = function(username, birthDate, id,course){
+   Person.call(this,username,birthDate);  // this is used for inheriting the similar things
+   this.course = course;
+   this.id = id;
+ }
 
-  PersonCl.prototype.calAge = function(){
-    this.DOB = 2025- this.DOB;
-    console.log(`${this.firstName} , your age is : ${this.DOB}`);
-  }
+ Student.prototype = Object.create(Person.prototype);
+ Student.prototype.intro = function(){
+  console.log(`The name of id of student is ${this.id} and currently studying ${this.course} `)
+ }
 
-  diyan.calAge();
-  
-  
+ const Ram = new Student('ram Bhatta',2005,23,'computer engineering');
+ Ram.intro();
+ Ram.calAge();   // now this Ram student can access to the calAge of Person.prototype
+ 
+
 
 
 
